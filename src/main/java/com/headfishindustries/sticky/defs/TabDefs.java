4b047d1a6b4be5f25b4b3b86e8e4b64aada44a48
TheIcyOne <icy.headfishindustries@gmail.com>
2017-04-18 02:20:07 +0100

    First release state
    Added textures for all trees, 3 lang files and a pretty little logo.

--- src/main/java/com/headfishindustries/sticky/defs/TabDefs.java
+++ src/main/java/com/headfishindustries/sticky/defs/TabDefs.java
@@ -1,10 +1,8 @@
 package com.headfishindustries.sticky.defs;
 
-import com.headfishindustries.sticky.blocks.leaves.LeavesMonster;
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
 public class TabDefs {
-	public static final CreativeTabs tabStickyTrees = new CreativeTabs("sticky.Trees") {public ItemStack getTabIconItem(){return new ItemStack(new LeavesMonster());}};
+	public static final CreativeTabs tabStickyTrees = new CreativeTabs("sticky.Trees") {public ItemStack getTabIconItem(){return new ItemStack(BlockDefs.saplingRainbow);}};
 }
